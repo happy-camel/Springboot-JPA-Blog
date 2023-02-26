e2e12e9d409e7023c4fc4b7ffe6f63a52a3450bb
happy-camel <acy2023@gmail.com>
2023-02-26 10:10:49 +0900

    글목록 페이징

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -3,6 +3,9 @@ package com.cos.blog.controller;
 import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,12 +20,12 @@ public class BoardController {
 //    컨트롤러에서 세션을 어떻게 찾?
 //    @AuthenticationPrincipal PrincipalDetail principal
     @GetMapping({"", "/"})
-    public String index(Model model) {
+    public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
 //        WEB-INF/views/index.jsp
 
 //        System.out.println("로그인 사용자 아이디 : " +principal.getUsername());
         //index페이지로 boards값이 들어간다
-        model.addAttribute("boards", boardService.글목록());
+        model.addAttribute("boards", boardService.글목록(pageable));
         return "index";//컨트롤러는 리턴시 viewResolver작동
     }
 
